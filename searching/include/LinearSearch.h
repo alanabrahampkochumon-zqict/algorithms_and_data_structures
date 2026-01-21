6b03e8b3d98993fa3613cec85ec0d3492561e81f
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-01-21 08:45:46 +0530

    Fixed typo in Searching CMake

--- searching/include/LinearSearch.h
+++ searching/include/LinearSearch.h
@@ -1,12 +1,14 @@
 #pragma once
 
+#include <cstddef>
+
 namespace Algorithms
 {
 	namespace Searching {
 
 
-		template<typename T, std::size_t
-		void linearSearch();
+		template<typename T, std::size_t Size>
+		std::size_t linearSearch(const T (&array)[Size], const T& element);
 	}
 }
 
