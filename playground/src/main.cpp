45a37364d49188cb330b2bbed8def3a6539ffd93
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-01-21 07:47:48 +0530

    Update Playground

--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -1,6 +1,52 @@
+#include <functional>
 #include <iostream>
 
 #include <InsertionSort.h>
+
+// Function Declarations
+
+template<typename T>
+void getInput(const int& limit, std::vector<T>& result);
+
+template<typename T>
+void printVector(const std::vector<T>& vec);
+
+static void printMainMenu();
+static void printSortingMenu();
+static void startApplication();
+
+
+
+
+// Struct and Classes
+
+struct MenuItem
+{
+	std::string label;
+	std::function<void()> action;
+
+	
+};
+
+static std::ostream& operator<<(std::ostream& os, const MenuItem& item)
+{
+	os << item.label;
+	return os;
+}
+
+
+// Global Variables
+
+MenuItem mainMenuItems[] = {
+	{"1. Sorting", printSortingMenu}
+};
+
+MenuItem sortingMenuItems[2] = {
+	// {"1. Sorting", Algorithms::InsertionSort::sort<int>}
+};
+
+
+// Function Definitions
 
 template <typename T>
 void getInput(const int& limit, std::vector<T>& result)
@@ -27,28 +73,39 @@ static void printMainMenu()
 {
 	std::cout << " ------------------------------------\n";
 	std::cout << "|            Algorithms              |\n";
-	std::cout << " ------------------------------------\n\n\n";
+	std::cout << " ------------------------------------\n\n";
 	std::cout << "Select an option from the list\n";
-	std::cout << "1. Sorting\n";
+
+	bool running = true;
+
+	for (auto& item: mainMenuItems)
+	{
+		std::cout << item << "\n";
+	}
+	while (running)
+	{
+		unsigned char option;
+		std::cin >> option;
+
+		if (option && option > 0 && option <= std::size(mainMenuItems))
+			mainMenuItems[option].action();
+	}
 }
 
 static void printSortingMenu()
 {
 	std::cout << " ------------------------------------\n";
 	std::cout << "|        Sorting Algorithms          |\n";
-	std::cout << " ------------------------------------\n\n\n";
+	std::cout << " ------------------------------------\n\n";
 	std::cout << "Select an option from the list\n";
 	std::cout << "1. Insertion Sort\n";
 	std::cout << "999. Go Back\n";
 }
 
-int main()
+
+
+static void startApplication(bool& running)
 {
-	std::cout << "+++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
-	std::cout << "|        Welcome to CPP Algorithm Playground          |\n";
-	std::cout << "+++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\n\n";
-	
-	bool running = true;
 
 	while (running)
 	{
@@ -56,4 +113,20 @@ int main()
 		printSortingMenu();
 		running = false;
 	}
+}
+
+static void printHeader()
+{
+	std::cout << "+++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
+	std::cout << "|        Welcome to CPP Algorithm Playground          |\n";
+	std::cout << "+++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\n\n";
+}
+
+int main()
+{
+	printHeader();
+
+	bool running = true;
+
+	startApplication(running);
 }
