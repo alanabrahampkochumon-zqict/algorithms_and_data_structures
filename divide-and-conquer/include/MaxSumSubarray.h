a3586fb89cbc8cec5757c207cf951aeadba58e1b
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-02-24 08:35:26 +0530

    Upgrade to cpp20

---
+++ divide-and-conquer/include/MaxSumSubarray.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cstddef>
+#include <span>
+#include <tuple>
+#include <concepts>
+
+namespace Algorithms::DivideAndConquer
+{
+	
+	template<typename T>
+	concept Arithmetic = std::integral<T> || std::floating_point<T>;
+
+	enum AlgorithmType: unsigned char
+	{
+		BRUTE_FORCE,
+		DIVIDE_AND_CONQUER,
+		HYBRID
+	};
+
+	template<Arithmetic T>
+	std::tuple<std::size_t, std::size_t, T> maxSumSubarray(std::span<T> elements);
+
+}
