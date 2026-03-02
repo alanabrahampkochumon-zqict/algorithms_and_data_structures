e344bc91e6b17a12452e777e620380b63bee6a96
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-02 08:04:45 +0530

    add benchmark for calculate k for hybrid max-sum-subarray algorithm

--- benchmark-suite/src/DivideAndConquer/MaxSumSubArrayBenchmarks.cpp
+++ benchmark-suite/src/DivideAndConquer/MaxSumSubArrayBenchmarks.cpp
@@ -8,18 +8,50 @@ using namespace Helpers;
 
 using namespace Algorithms::DivideAndConquer;
 
-template<AlgorithmType Algorithm>
-static void BM_MaxSumSubarray(benchmark::State &state)
+namespace
 {
-	std::size_t size = state.range(0);
-	std::vector vector = generateRandomVector<int>(size);
+	template<AlgorithmType Algorithm>
+	void BM_MaxSumSubarray(benchmark::State& state)
+	{
+		std::size_t size = state.range(0);
+		std::vector vector = generateRandomVector<int>(size);
 
-	for (auto _ : state)
-		benchmark::DoNotOptimize(maxSumSubarray(vector, vector.size(), Algorithm));
+		for (auto _ : state)
+			benchmark::DoNotOptimize(maxSumSubarray(vector, vector.size(), Algorithm));
+	}
+
+	BENCHMARK_TEMPLATE(BM_MaxSumSubarray, BRUTE_FORCE)->RangeMultiplier(2)->Range(8, 8192);
+	BENCHMARK_TEMPLATE(BM_MaxSumSubarray, DIVIDE_AND_CONQUER)->RangeMultiplier(2)->Range(8, 8192);
+	BENCHMARK_TEMPLATE(BM_MaxSumSubarray, HYBRID)->RangeMultiplier(2)->Range(8, 8192);
+
+	//void BM_MaxSumSubarray_Hybrid(benchmark::State& state)
+	//{
+	//	std::size_t size = state.range(0);
+	//	std::size_t limit = state.range(1);
+
+	//	std::vector vector = generateRandomVector<int>(size);
+
+	//	for (auto _ : state)
+	//		benchmark::DoNotOptimize(maxSumSubarray(vector, vector.size(), HYBRID, limit));
+	//}
+
+	//BENCHMARK(BM_MaxSumSubarray_Hybrid)->RangeMultiplier(2)->Ranges({ { 8, 8192 }, { 10, 80 } });
+
+
+	template <AlgorithmType Algorithm>
+	void BM_MaxSumSubarray_Hybrid_K(benchmark::State& state)
+	{
+		std::size_t limit = state.range(0);
+
+		std::vector vector = generateRandomVector<int>(16000);
+
+		for (auto _: state)
+		{
+			benchmark::DoNotOptimize(maxSumSubarray(vector, vector.size(),Algorithm, limit));
+		}
+	}
+
+	BENCHMARK_TEMPLATE(BM_MaxSumSubarray_Hybrid_K, DIVIDE_AND_CONQUER)->DenseRange(2, 2, 2); // Baseline
+	BENCHMARK_TEMPLATE(BM_MaxSumSubarray_Hybrid_K, HYBRID)->DenseRange(10, 80, 2);
+
 }
-
-
-
-BENCHMARK_TEMPLATE(BM_MaxSumSubarray, BRUTE_FORCE)->RangeMultiplier(2)->Range(8, 8192);
-BENCHMARK_TEMPLATE(BM_MaxSumSubarray, DIVIDE_AND_CONQUER)->RangeMultiplier(2)->Range(8, 8192);
-BENCHMARK_TEMPLATE(BM_MaxSumSubarray, HYBRID)->RangeMultiplier(2)->Range(8, 8192);
