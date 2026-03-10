15d119c7390788be4f5f833b9463a513b6bb34ea
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-10 08:49:37 +0530

    Add doc to benchmark + refactor

--- benchmark-suite/include/helpers/RandomGenerators.h
+++ benchmark-suite/include/helpers/RandomGenerators.h
@@ -1,40 +1,56 @@
 #pragma once
+/**
+ * @file RandomGenerators.h
+ * @author Alan Abraham P Kochumon
+ * @date Created on: February 28, 2026
+ *
+ * @brief Random Generator Utilities for Benchmarking.
+ *
+ * @par Utilities
+ * Random Number Generator
+ * Random Vector Generator(std::vector)
+ *
+ * @copyright Copyright (c) 2026 Alan Abraham P Kochumon
+ */
+
 
 #include <chrono>
-#include <random>
 #include <concepts>
+#include <random>
 #include <vector>
 
-namespace Helpers
+namespace rng::helpers
 {
-	template <typename T>
-		requires std::integral<T> || std::floating_point<T>
-	T generateRandom(T min = T(1), T max = T(1000), std::size_t seed = std::chrono::system_clock::now().time_since_epoch().count())
-	{
-		thread_local std::mt19937 engine(static_cast<unsigned int>(seed));
+    template <typename T>
+        requires std::integral<T> || std::floating_point<T>
+    T generateRandom(T min = T(1), T max = T(1000),
+                     std::size_t seed = std::chrono::system_clock::now().time_since_epoch().count())
+    {
+        thread_local std::mt19937 engine(static_cast<unsigned int>(seed));
 
 
-		if constexpr (std::is_integral_v<T>)
-		{
-			std::uniform_int_distribution<T> dist(min, max);
-			return dist(engine);
-		}
-		else
-		{
-			std::uniform_real_distribution<T> dist(min, max);
-			return dist(engine);
-		}
+        if constexpr (std::is_integral_v<T>)
+        {
+            std::uniform_int_distribution<T> dist(min, max);
+            return dist(engine);
+        }
+        else
+        {
+            std::uniform_real_distribution<T> dist(min, max);
+            return dist(engine);
+        }
+    }
 
-	}
-	template <typename T>
-		requires std::integral<T> || std::floating_point<T>
-	std::vector<T> generateRandomVector(std::size_t size, T min = T(-100), T max = T(1000))
-	{
-		std::vector<T> vec(size);
 
-		for (auto& v: vec)
-			v = generateRandom(min, max);
+    template <typename T>
+        requires std::integral<T> || std::floating_point<T>
+    std::vector<T> generateRandomVector(std::size_t size, T min = T(-100), T max = T(1000))
+    {
+        std::vector<T> vec(size);
 
-		return vec;
-	}
-}
+        for (auto& v : vec)
+            v = generateRandom(min, max);
+
+        return vec;
+    }
+} // namespace rng::helpers
--- benchmark-suite/src/DivideAndConquer/MaxSumSubArrayBenchmarks.cpp
+++ benchmark-suite/src/DivideAndConquer/MaxSumSubArrayBenchmarks.cpp
@@ -1,58 +1,68 @@
-#include <MaxSumSubarray.h>
+/**
+ * @file MaxSumSubArrayBenchmarks.cpp
+ * @author Alan Abraham P Kochumon
+ * @date Created on: February 28, 2026
+ *
+ * @brief MaxSumSubarray benchmark for assessing performance of different algorithm types.
+ *
+ * @copyright Copyright (c) 2026 Alan Abraham P Kochumon
+ */
 
+
+#include "helpers/RandomGenerators.h"
+
+#include <MaxSumSubarray.h>
 #include <benchmark/benchmark.h>
 
-#include "../../include/helpers/RandomGenerators.h"
-
-using namespace Helpers;
+using namespace rng::helpers;
 
 using namespace algorithms;
 
 namespace
 {
-	template<AlgorithmType Algorithm>
-	void BM_MaxSumSubarray(benchmark::State& state)
-	{
-		std::size_t size = state.range(0);
-		std::vector vector = generateRandomVector<int>(size);
+    template <AlgorithmType Algorithm>
+    void BM_MaxSumSubarray(benchmark::State& state)
+    {
+        std::size_t size = state.range(0);
+        std::vector vector = generateRandomVector<int>(size);
 
-		for (auto _ : state)
-			benchmark::DoNotOptimize(maxSumSubarray(vector, vector.size(), Algorithm));
-	}
+        for (auto _ : state)
+            benchmark::DoNotOptimize(maxSumSubarray(vector, vector.size(), Algorithm));
+    }
 
-	BENCHMARK_TEMPLATE(BM_MaxSumSubarray, BRUTE_FORCE)->RangeMultiplier(2)->Range(8, 8192); // theta(n^2)
-	BENCHMARK_TEMPLATE(BM_MaxSumSubarray, DIVIDE_AND_CONQUER)->RangeMultiplier(2)->Range(8, 8192); // theta(nlgn)
-	BENCHMARK_TEMPLATE(BM_MaxSumSubarray, HYBRID)->RangeMultiplier(2)->Range(8, 8192); // theta(nlgn) Optimized
-	BENCHMARK_TEMPLATE(BM_MaxSumSubarray, KADANES)->RangeMultiplier(2)->Range(8, 8192); // theta(n)
+    BENCHMARK_TEMPLATE(BM_MaxSumSubarray, BRUTE_FORCE)->RangeMultiplier(2)->Range(8, 8192);        // theta(n^2)
+    BENCHMARK_TEMPLATE(BM_MaxSumSubarray, DIVIDE_AND_CONQUER)->RangeMultiplier(2)->Range(8, 8192); // theta(nlgn)
+    BENCHMARK_TEMPLATE(BM_MaxSumSubarray, HYBRID)->RangeMultiplier(2)->Range(8, 8192);  // theta(nlgn) Optimized
+    BENCHMARK_TEMPLATE(BM_MaxSumSubarray, KADANES)->RangeMultiplier(2)->Range(8, 8192); // theta(n)
 
-	//void BM_MaxSumSubarray_Hybrid(benchmark::State& state)
-	//{
-	//	std::size_t size = state.range(0);
-	//	std::size_t limit = state.range(1);
+    // void BM_MaxSumSubarray_Hybrid(benchmark::State& state)
+    //{
+    //	std::size_t size = state.range(0);
+    //	std::size_t limit = state.range(1);
 
-	//	std::vector vector = generateRandomVector<int>(size);
+    //	std::vector vector = generateRandomVector<int>(size);
 
-	//	for (auto _ : state)
-	//		benchmark::DoNotOptimize(maxSumSubarray(vector, vector.size(), HYBRID, limit));
-	//}
+    //	for (auto _ : state)
+    //		benchmark::DoNotOptimize(maxSumSubarray(vector, vector.size(), HYBRID, limit));
+    //}
 
-	//BENCHMARK(BM_MaxSumSubarray_Hybrid)->RangeMultiplier(2)->Ranges({ { 8, 8192 }, { 10, 80 } });
+    // BENCHMARK(BM_MaxSumSubarray_Hybrid)->RangeMultiplier(2)->Ranges({ { 8, 8192 }, { 10, 80 } });
 
 
-	template <AlgorithmType Algorithm>
-	void BM_MaxSumSubarray_Hybrid_K(benchmark::State& state)
-	{
-		std::size_t limit = state.range(0);
+    template <AlgorithmType Algorithm>
+    void BM_MaxSumSubarray_Hybrid_K(benchmark::State& state)
+    {
+        std::size_t limit = state.range(0);
 
-		std::vector vector = generateRandomVector<int>(16000);
+        std::vector vector = generateRandomVector<int>(16000);
 
-		for (auto _: state)
-		{
-			benchmark::DoNotOptimize(maxSumSubarray(vector, vector.size(),Algorithm, limit));
-		}
-	}
+        for (auto _ : state)
+        {
+            benchmark::DoNotOptimize(maxSumSubarray(vector, vector.size(), Algorithm, limit));
+        }
+    }
 
-	BENCHMARK_TEMPLATE(BM_MaxSumSubarray_Hybrid_K, DIVIDE_AND_CONQUER)->DenseRange(2, 2, 2); // Baseline
-	BENCHMARK_TEMPLATE(BM_MaxSumSubarray_Hybrid_K, HYBRID)->DenseRange(10, 80, 2);
+    BENCHMARK_TEMPLATE(BM_MaxSumSubarray_Hybrid_K, DIVIDE_AND_CONQUER)->DenseRange(2, 2, 2); // Baseline
+    BENCHMARK_TEMPLATE(BM_MaxSumSubarray_Hybrid_K, HYBRID)->DenseRange(10, 80, 2);
 
-}
+} // namespace
